c25509bc654d7a3b4fa2bc06120f62f3030fe0b8
zeross11 <159096446+zeross11@users.noreply.github.com>
2026-07-30 22:10:39 -0700

    fix: retire legacy MineReady service-worker caches

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,2 @@
 self.addEventListener('install',function(){self.skipWaiting();});
-self.addEventListener('activate',function(event){event.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.map(function(key){return caches.delete(key);}));}).then(function(){return self.registration.unregister();}));});
+self.addEventListener('activate',function(event){event.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(key){return key.indexOf('mineready-')===0;}).map(function(key){return caches.delete(key);}));}).then(function(){return self.registration.unregister();}).then(function(){return self.clients.matchAll({type:'window'});}).then(function(clients){clients.forEach(function(client){client.navigate(client.url);});}));});
